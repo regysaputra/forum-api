57bea7322973ad04f1c6836b24384df669b40d91
regysaputra <regysaputra27@gmail.com>
2024-03-23 14:51:36 +0000

    add ecosystem file for pm2 & production environment file

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name   : "forum-api",
+    script : "npm",
+    args   : "run start",
+    env_production: {
+	NODE_ENV: "production"
+    },
+    env_development: {
+	NODE_ENV: "development"
+    }
+  }]
+}
